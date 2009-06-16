b85e87ed9eb739dda1398d2e49d54d5464ee485f
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-06-16 12:53:19 +0000

    enabled sfGuard

--- trunk/lib/filter/base/BaseBanishmentFormFilter.class.php
+++ trunk/lib/filter/base/BaseBanishmentFormFilter.class.php
@@ -18,12 +18,16 @@ class BaseBanishmentFormFilter extends BaseFormFilterPropel
       'character_id'    => new sfWidgetFormPropelChoice(array('model' => 'Character', 'add_empty' => true)),
       'banished_until'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
       'banished_for_id' => new sfWidgetFormFilterInput(),
+      'banished_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
+      'level'           => new sfWidgetFormFilterInput(),
     ));
 
     $this->setValidators(array(
       'character_id'    => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Character', 'column' => 'id')),
       'banished_until'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
       'banished_for_id' => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'banished_at'     => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
+      'level'           => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
     ));
 
     $this->widgetSchema->setNameFormat('banishment_filters[%s]');
@@ -45,6 +49,8 @@ class BaseBanishmentFormFilter extends BaseFormFilterPropel
       'character_id'    => 'ForeignKey',
       'banished_until'  => 'Date',
       'banished_for_id' => 'Number',
+      'banished_at'     => 'Date',
+      'level'           => 'Number',
     );
   }
 }
