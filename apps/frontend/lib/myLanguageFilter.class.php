215ed80ab786cbe629fa9fe886c130d24a2bce5f
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-06-21 18:00:17 +0000

    changed url format from host/culture to culture.host

--- apps/frontend/lib/myLanguageFilter.class.php
+++ apps/frontend/lib/myLanguageFilter.class.php
@@ -5,17 +5,24 @@
 
     public function execute ($filterChain)
     {    
-      // execute this filter only once
-      if (preg_match("/^(hu|en)/i", $this->getContext()->getRequest()->getHost(), $matches)) {
+      if (preg_match("/^(hu|en)\\./i", $this->getContext()->getRequest()->getHost(), $matches)) {
         $this->getContext()->getUser()->setCulture($matches[1]);
         $this->getContext()->getUser()->isFirstRequest(false);
       }
-
+      
       if ($this->getContext()->getUser()->isFirstRequest())
       {
         $culture = $this->getContext()->getRequest()->getPreferredCulture(array("hu", "en"));
         $this->getContext()->getUser()->setCulture($culture);
         $this->getContext()->getUser()->isFirstRequest(false);
+      }
+      
+      if (!preg_match("/^(hu|en)\\./i", $this->getContext()->getRequest()->getHost())) {
+        $uri = $this->getContext()->getRequest()->getUri();
+        $host = $this->getContext()->getRequest()->getHost();
+        $culture = $this->getContext()->getUser()->getCulture();
+        $redir = str_replace("/{$host}/", "/{$culture}.{$host}/", $uri);
+        $this->getContext()->getController()->redirect($redir);
       }
       // execute next filter
       $filterChain->execute();
