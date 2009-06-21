6f66ee95d87bdbcbe235ab90933c551df4d3b7c8
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-06-21 14:34:20 +0000

    added TibiaWebsite::getActivePolls()

--- lib/temp.php
+++ lib/temp.php
@@ -4,7 +4,8 @@ require("TibiaWebsite.class.php");
 require("RemoteFile.class.php");
 require("Tibiahu.class.php");
 
-echo("<pre>");print_r(
-//var_dump(
-TibiaWebsite::getFeaturedArticle());
+//echo("<pre>");print_r(
+var_dump(
+TibiaWebsite::getActivePolls()
+);
 
