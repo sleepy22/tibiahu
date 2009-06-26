d5f01971a365dff28da3153d28d4b84daad65b1a
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-06-26 21:38:21 +0000

    created news and newscategory backend modules

--- trunk/apps/backend/lib/myUser.class.php
+++ trunk/apps/backend/lib/myUser.class.php
@@ -1,5 +1,18 @@
 <?php
 
-class myUser extends sfBasicSecurityUser
+class myUser extends sfGuardSecurityUser
 {
+  
+  public function isFirstRequest($boolean = null)
+  {
+    if (is_null($boolean))
+    {
+      return $this->getAttribute('first_request', true);
+    }
+    else
+    {
+      $this->setAttribute('first_request', $boolean);
+    }
+  }
+  
 }
