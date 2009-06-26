b7565128b581e77e8f2c804540961741215809e2
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-06-26 21:38:21 +0000

    created news and newscategory backend modules

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Tibia.hu - Admin</title>
     <link rel="shortcut icon" href="/favicon.ico" />
+    <?php use_stylesheet('admin.css') ?>
+    <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+      <div id="header">
+        <h1>
+          Tibia.hu admin
+        </h1>
+      </div>
+      <?php if ($sf_user->isAuthenticated()) : ?>
+      <div id="menu">
+        <ul>
+          <li><?php echo link_to('News', '@news') ?></li>
+          <li><?php echo link_to('News categories', '@news_category') ?></li>
+          <li><?php echo link_to('Users', '@sf_guard_user') ?></li>
+          <li><?php echo link_to('Logout', '@sf_guard_signout') ?></li>
+        </ul>
+      </div>
+      <?php endif; ?>
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+ 
+      <div id="footer">
+      </div>
+    </div>
   </body>
 </html>
