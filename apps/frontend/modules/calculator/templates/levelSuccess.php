972a9675df6b2b74b2087a6b205efef432c5750e
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-07-03 13:16:14 +0000

    improved the level calculator design and functionality

--- apps/frontend/modules/calculator/templates/levelSuccess.php
+++ apps/frontend/modules/calculator/templates/levelSuccess.php
@@ -15,33 +15,37 @@
     <?php if (isset($level_current)):  #ha volt ervenyes kuldott adat, es szamoltunk ?>
     <?php use_helper("Number") ?>
     <p>
-      <?php echo __("Ennyi tapasztalattal az aktuális szinted <b>%level_current%</b>, "
+      <?php echo __("Ennyi tapasztalattal az aktuális szinted <b>%level_current%</b>,<br /> "
       . "a következő szinthez <b>%xp_next%</b> xp kell, ebből <b>%xp_remaining%</b> van még hátra.",
       array(
         "%level_current%" =>  format_number($level_current),
         "%xp_next%"       =>  format_number($xp_next),
         "%xp_remaining%"  =>  format_number($xp_remaining)
-      )) ?>
+      ), "calculators") ?>
       <br />
-      <?php echo __("Szörnyszerűsítve ez így néz ki:") ?>
+      <?php echo __("Szörnyszerűsítve ez így néz ki:", array(), "calculators") ?>
     </p>
     
-    <table class="levelhistory">
+    <table border="1">
       <thead>
         <tr>
-          <th><?php __("Szörny") ?></th>
-          <th><?php __("Darabszám") ?></th>
+          <th><?php echo __("Szörny") ?></th>
+          <th><?php echo __("Darabszám") ?></th>
         </tr>
       </thead>
       <tbody>
         <?php foreach ($monsters as $monster => $count): ?>
         <tr>
           <td><?php echo link_to($monster,"http://tibia.wikia.com/wiki/Special:Search?search=".urlencode($monster)) ?></td>
-          <td><?php echo $count ?></td>
+          <td class="center"><?php echo $count ?></td>
         </tr>
         <?php endforeach; ?>
       </tbody>
     </table>  
+    
+   <?php echo __("Experience share minimum szint", array(), "character") ?>: <b><?php echo $xpshare_levels["min"] ?></b><br />
+   <?php echo __("Experience share maximum szint", array(), "character") ?>: <b><?php echo $xpshare_levels["max"] ?></b><br />
+    
     <?php endif; ?>
     
   </div>
