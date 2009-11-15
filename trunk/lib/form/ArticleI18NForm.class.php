8fd03372e655d9d99c8bf3f79fb3679faa0ed0b3
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-11-15 14:41:21 +0000

    adding previously forgotten files

---
+++ trunk/lib/form/ArticleI18NForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ArticleI18N form.
+ *
+ * @package    tibiahu
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfPropelFormTemplate.php 10377 2008-07-21 07:10:32Z dwhittle $
+ */
+class ArticleI18NForm extends BaseArticleI18NForm
+{
+  public function configure()
+  {
+  }
+}
