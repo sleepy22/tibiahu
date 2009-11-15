e117ce01b910352726da326c352f79870bb65aff
maerlyng <maerlyng@f8db92d4-5921-11de-a2a3-1fafad8a8996>
2009-11-15 14:41:21 +0000

    adding previously forgotten files

---
+++ lib/filter/ArticleI18NFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ArticleI18N filter form.
+ *
+ * @package    tibiahu
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class ArticleI18NFormFilter extends BaseArticleI18NFormFilter
+{
+  public function configure()
+  {
+  }
+}
